a27917b3027012ee130ccc2d1cabd5883bc6e802
Peritract <dan@getfutureproof.co.uk>
2023-01-17 11:22:18 +0000

    Add route for single goat

--- server/app.js
+++ server/app.js
@@ -10,12 +10,30 @@ const app = express(); // Make a very basic server using Express
 
 app.get("/", (req, res) => {
     res.json({
-        "message": "Hello, World!"
+        "message": "Welcome to the GOAT API!"
     })
 })
 
 app.get("/goats", (req, res) => {
     res.json(goats);
+})
+
+app.get("/goats/:id", (req, res) => {
+
+    const id = req.params["id"];
+
+    // Filter the goat list for the relevant goat
+    const goat = goats.filter(g => g["id"] == id)[0];
+
+    if (goat) {
+        // Send out the goat
+        res.json(goat);
+    } else {
+        // Send a status of 404 with a message
+        res.status(404).json({
+            error: "No such goat!"
+        })
+    }
 })
 
 module.exports = app; // Make the server available to other files
